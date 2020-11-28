aa29612526c5a497f0f32cac51c63c5f8501f473
Erik Shelton <erikshelton@Eriks-Air.lan1>
2020-11-27 20:39:05 -0500

    unauthorization to /loans fix

--- app/controllers/api/v1/loans_controller.rb
+++ app/controllers/api/v1/loans_controller.rb
@@ -1,7 +1,7 @@
 module Api 
   module V1
     class LoansController < ApplicationController
-      before_action :authorize_access_request! 
+      before_action :authorize_access_request!
       before_action :set_loan, only: [:show, :update, :destroy]
 
       # GET /loans
